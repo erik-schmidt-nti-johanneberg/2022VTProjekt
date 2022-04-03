c0701a99c899068edf8daed83a82f62db9fce73f
erik-schmidt-nti-johanneberg <94617304+erik-schmidt-nti-johanneberg@users.noreply.github.com>
2022-04-03 17:09:13 +0200

    allt är klart unless

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function myFunction() {
   }
 
 let slideIndex = 0;
-  showSlides();
+  
   
   function showSlides() {
     let i;
@@ -20,4 +20,18 @@ let slideIndex = 0;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 3000); // 3 seconds
+}
+
+function hideImage(element) {
+  element.style.display = "none";
+}
+
+function showImage(element) {
+  const imageDiv = document.getElementById("full-image-div");
+  imageDiv.style.display = "flex";
+  
+  const srcImage = element.getElementsByTagName('img');
+  const imageToShow = document.getElementById("full-image");
+  imageToShow.src = srcImage[0].src; 
+  imageToShow.alt = srcImage[0].alt; 
 }
